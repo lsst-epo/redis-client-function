327b1ffacacee7b37054f4d2e2c3d5672d00809b
Eric Rosas <ericdrosas@gmail.com>
2023-08-11 12:19:18 -0700

    Updated with stale-fallback and per-temporal basis EPO-8373

--- index.ts
+++ index.ts
@@ -3,33 +3,60 @@ import 'dotenv/config'
 import * as ff from '@google-cloud/functions-framework';
 
 ff.http('summit-status', async (req: ff.Request, res: ff.Response) => {
+    res.set('Access-Control-Allow-Origin', "*")
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
 
-    if(req.method == "POST") {
+    if (req.method === "OPTIONS") {
+        // Send response to OPTIONS requests
+        res.set('Access-Control-Allow-Methods', 'GET');
+        res.set('Access-Control-Allow-Headers', 'Content-Type');
+        res.set('Access-Control-Max-Age', '3600');
+        res.status(204).send('');
+        return;
+        
+    } else if(req.method == "POST") {
         console.log("SummitStatusDataUpdater: Saving Data!"); // Used for querying Logging Explorer to find the "SummitStatusDataUpdater"
 
         const client = await getClient();
         await client.connect();
 
-        await client.hSet('summit-status:current', req.body.digest.current);
-
-        await client.set("summit-status:daily", JSON.stringify(req.body.digest.daily));
-
-        await client.set('summit-status:hourly', JSON.stringify(req.body.digest.hourly));
-
-        return res.status(200).send("Saved data!");
+        if(req.path == "/current-stats") {
+            if(req.body.current == undefined) {
+                return res.status(204).json({ status: "SUCCESS", message: "No data to save! This usually means an error occurred while querying the EFD database."});
+            } else {
+                await client.set('summit-status:current', JSON.stringify(req.body.current));
+                return res.status(200).json({ status: "SUCCESS", message: "Saved current stats data!"});
+            }
+        } else if(req.path == "/hourly-stats") {
+            if(req.body.hourly == undefined) {
+                return res.status(204).json({ status: "SUCCESS", message: "No data to save! This usually means an error occurred while querying the EFD database."});
+            } else {
+                await client.set('summit-status:hourly', JSON.stringify(req.body.hourly));
+                return res.status(200).json({ status: "SUCCESS", message: "Saved hourly stats data!"});
+            }
+        } else if(req.path == "/daily-stats") {
+            if(req.body.daily == undefined) {
+                return res.status(204).json({ status: "SUCCESS", message: "No data to save! This usually means an error occurred while querying the EFD database."});
+            } else {
+                await client.set("summit-status:daily", JSON.stringify(req.body.daily));
+                return res.status(200).json({ status: "SUCCESS", message: "Saved daily stats data!"});
+            }
+        } else {
+            return res.status(404).json({ status: "ERROR", message: "Incorrect endpoint."});
+        }
 
     } else if(req.method == "GET") {
         const client = await getClient();
         await client.connect();
 
-        let currentSummitData = await client.hGetAll('summit-status:current');
+        let currentSummitData = await client.get('summit-status:current');
         let hourlySummitData = await client.get('summit-status:hourly');
         let dailySummitData = await client.get('summit-status:daily');
 
         let summitData = {
-            current: currentSummitData,
-            hourly: JSON.parse(String(hourlySummitData)),
-            daily: JSON.parse(String(dailySummitData))
+            current: (currentSummitData == null) ? { error: "No data available." } : JSON.parse(currentSummitData),
+            hourly: (hourlySummitData == null) ? { error: "No data available." } : JSON.parse(hourlySummitData),
+            daily: (dailySummitData == null) ? { error: "No data available." } : JSON.parse(dailySummitData)
         }
         return res.status(200).send(summitData);
     } else {
