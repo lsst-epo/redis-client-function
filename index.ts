f3eacc6165c0faabc60cef12130437b1fb4c687a
Kevin Wong <kevin.wong@noirlab.edu>
2026-01-26 14:02:01 -0700

    revert to singular in /widgets response keys

--- index.ts
+++ index.ts
@@ -132,7 +132,7 @@ ff.http('summit-status', async (req: ff.Request, res: ff.Response) => {
                 weather: { 
                     pictocode: summitData.rawCurrentWeather?.data_current?.pictocode_detailed ?? 0 
                 },
-                exposures: { 
+                exposure: { 
                     count: summitData.nightlyDigest?.exposure_count ?? 0 
                 },
                 dome: { 
@@ -141,7 +141,7 @@ ff.http('summit-status', async (req: ff.Request, res: ff.Response) => {
                 survey: {
                     progress: summitData.survey?.progress ?? 0
                 },
-                alerts: {
+                alert: {
                     count: summitData.alert?.count ?? 0
                 }
             }
